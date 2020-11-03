42d5a6c56be562299aebed271d06fe201ec76d25
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2020-11-03 21:37:18 +0000

    Changing error hadnling message

--- src/cam_control.py
+++ src/cam_control.py
@@ -9,13 +9,12 @@ import dropbox_integration as dbx_int
 def take_picture():
     output_path = 'jpg/image.jpg'
     try:
-        output_path = 'jpg/image.jpg'
         camera = PiCamera()
         camera.rotation=180
         camera.start_preview()
         camera.capture('static/' + output_path)
     except: 
-        print('I could not take a picture')
+        print('Camera failed to take a picture')
     finally:
         camera.close()
 
