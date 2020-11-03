e7509dda8bcbf0c6957c90c5cd01b2153b7569ad
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2020-11-03 22:02:25 +0000

    Add file upload to dropbox

--- src/dropbox_integration.py
+++ src/dropbox_integration.py
@@ -1,5 +1,6 @@
 import dropbox
 import configparser
+from datetime import datetime
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -8,3 +9,14 @@ config.read('config.ini')
 dbx = dropbox.Dropbox(config['Dropbox']['AccessToken'])
 
 del config
+
+
+def file_upload(file_from):
+
+    #add session name
+
+    now = datetime.now()
+    file_to = '/image' + now.strftime("%Y_%m_%d_%H_%M_%S") + '.jpg'
+    
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to)
