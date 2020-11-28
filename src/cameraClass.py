71d606ce9c45a27a14caeb4cdb4780769b89821f
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2020-11-28 19:12:39 +0100

    Issue10: Improve motion detection

--- src/cameraClass.py
+++ src/cameraClass.py
@@ -8,43 +8,39 @@ import time
 from src.boardClass import *
 from src.dartThrowClass import *
 
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
 class Camera:
         
-    def __init__(self, name, rotation, width = 720, height = 480):
+    def __init__(self, name, rotation, width = 640, height = 480):
         self.name = name
         self.rotation = rotation
-        self.board = self.calibration()
         self.width = width
         self.height = height
+        self.rel_pts = self.calibration()
+        self.bnds = self.set_camera_bnds()
+        self.board = Board(rel_pts = self.rel_pts)
         self.dartThrow = None
-        self.bnds = self.get_camera_bnds() # set image boundaries around dart board dependent on calibration
 
     def calibration(self):
         rel_pts = {
-            "center": [372,318],
-            "left": [221,333],
-            "right": [525,301],
-            "top": [360,217],
-            "bottom": [380,372]
+            "center": [338,323],
+            "left": [205,336],
+            "right": [472,308],
+            "top": [324,234],
+            "bottom": [345,368]
         }
-
-        return Board(rel_pts = rel_pts)
+        return rel_pts
 
     def motion_detection(self):
-
-        image_before_link = 'static/jpg/before.jpg'
-        image_after_link = 'static/jpg/after.jpg'
         
         t_repeat = 0.05 # Take a picure every t_repeat seconds
         t_max = 0.2 # Maximum time the motion should take time - hereby we can distinguish between dart throw and human
+        t_pause = 1 # Time to pause after larger object or longer movement was detected (e.g. hand)
 
         images = [0,0,0] # indexes: 0: before motion, 1: motion detected?, 2: after motion
 
-                             
+        minThres = 0.0002 #Thresholds important - make accessible / dynamic - between 0 and 1
+        maxThres = 0.01
+
         while True:
 
             #1. Step Check for motion
@@ -55,8 +51,16 @@ class Camera:
             
             if isinstance(images[0],int):
                 continue
+            
+            #C heck if object size is plausible
+            img_diff_ratio = Camera.get_img_diff_ratio(images[0], images[1])
 
-            if Camera.are_images_different(images[0], images[1]):
+            if img_diff_ratio > maxThres:
+                # Object too large
+                time.sleep(t_pause)
+                continue
+            elif img_diff_ratio > minThres and img_diff_ratio < maxThres:
+                # Motion detected and no large object
                 t = t_repeat
 
                 # 2. Step: Check if motion stops within time
@@ -64,23 +68,42 @@ class Camera:
                     time.sleep(t_repeat)
                     img = self.take_picture()
                     images[2] = img
-
-                    if Camera.are_images_different(images[1], images[2]):
+                    
+                    #Check if there is still movement
+                    img_diff_ratio = Camera.get_img_diff_ratio(images[1], images[2])
+                    if img_diff_ratio > minThres: # Check if motion is still ongoing
                         t = t + t_repeat
                         continue
                     else: # Motion stopped
                         break
+                
+                # Check if motion stopped in time
+                if t < t_max:
+                     # Final check if object size is plausible for a Dart
+                    img_diff_ratio = Camera.get_img_diff_ratio(images[0], images[2])
+                    if img_diff_ratio > minThres and img_diff_ratio < maxThres:
+                        t = t_repeat
+                        print('Detected')
+                        # Get image output path
+                        config = configparser.ConfigParser()
+                        config.read('config.ini')
+                        image_before_link = config['Paths']['image_before_link']
+                        image_after_link = config['Paths']['image_after_link']
+                        del config
 
-                if t < t_max: # Motion detected and stopped in time
-                    cv2.imwrite(image_before_link,images[0])
-                    cv2.imwrite(image_after_link,images[2])
-                    break
+                        cv2.imwrite(image_before_link, images[0])
+                        cv2.imwrite(image_after_link, images[2])
+                        break
+
+                    else:
+                        continue
+
                 else: #Motion detected but was too long
-                    time.sleep(1)
+                    print('Too long')
+                    time.sleep(t_pause)
                     images = [0,0,0]
                     continue
-            
-        print('Detected')
+
         self.dartThrow = dartThrow(image_before_link,image_after_link,self.board)
 
     # takes one picture and returns it
@@ -90,14 +113,14 @@ class Camera:
             # define a video capture object 
             cap = cv2.VideoCapture(0)
             
-            #set the width and height, and UNSUCCESSFULLY set the exposure time
-            #cap.set(3,self.width)
-            #cap.set(4,self.height) 
+            #set the width and height
+            cap.set(3,self.width)
+            cap.set(4,self.height) 
 
             ret, img = cap.read() 
             img = cv2.rotate(img, cv2.ROTATE_180)  #Rotation important, make dynamic / accessible
 
-            # Crop to dart board
+            # Crop image around dart board
             img = img[self.bnds['top']:self.bnds['bottom'], self.bnds['left']:self.bnds['right']]
 
         except: 
@@ -110,34 +133,37 @@ class Camera:
 
         return img
 
-    def get_camera_bnds(self):
+    def set_camera_bnds(self):
 
         extend = 100
-        bnds = {'top': max(self.board.rel_pts['top'][1] - extend, 0),
-                'left': max(self.board.rel_pts['left'][0] - extend, 0),
-                'right': min(self.board.rel_pts['right'][0] + extend, self.width),
-                'bottom':min(self.board.rel_pts['bottom'][0] + extend, self.height)
+        bnds = {'top': max(self.rel_pts['top'][1] - extend, 0),
+                'left': max(self.rel_pts['left'][0] - extend, 0),
+                'right': min(self.rel_pts['right'][0] + extend, self.width),
+                'bottom':min(self.rel_pts['bottom'][0] + extend, self.height)
                 }
+
+        # adjust relative points
+        shift = [bnds['left'],bnds['top']]
+        for k,v in self.rel_pts.items():
+            self.rel_pts[k] = [a - b for a, b in zip(self.rel_pts[k], shift)]
 
         return bnds
 
     @staticmethod
-    def are_images_different(img1, img2):
-
-        minThres =  80 #Thresholds important - make accessible / dynamic
-        maxThres = 1000
+    def get_img_diff_ratio(img1, img2):
 
         diff = cv2.absdiff(img2,img1)
-        diffGray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+        diff = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
 
-        blur = cv2.GaussianBlur(diffGray, (5, 5), 0)
+        diff = cv2.GaussianBlur(diff, (5, 5), 0)
         #blur = cv2.bilateralFilter(blur, 9, 75, 75)
-        _, thresh = cv2.threshold(blur, 60, 255, 0)
+        _, thresh = cv2.threshold(diff, 60, 255, 0)
 
-        print(cv2.countNonZero(thresh))
+        white_pixels = cv2.countNonZero(thresh)
+        total_pixels = diff.size
+        ratio = white_pixels/total_pixels
 
-        if (cv2.countNonZero(thresh) > minThres and cv2.countNonZero(thresh) < maxThres):
-            return True
-        else:
-            return False
+        print(ratio)
+
+        return ratio
 
