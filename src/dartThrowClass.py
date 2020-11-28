a4310e0504b02acc5ec06b6e21a7c27e44994d13
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2020-11-28 19:13:15 +0100

    Issue10: Remove isdart out of dartThrow class

--- src/dartThrowClass.py
+++ src/dartThrowClass.py
@@ -7,19 +7,13 @@ class dartThrow:
         self.img_before_link = img_before_link
         self.img_after_link = img_after_link
         self.board = board
-        self.is_dart = self.is_dart()
-
-        if self.is_dart:
-            self.rel_carth_pos = self.get_rel_pos()
-            self.score = self.board.get_score(self.rel_carth_pos)
+        
+        self.rel_carth_pos = self.get_rel_pos()
+        self.score = self.board.get_score(self.rel_carth_pos)
 
     def __repr__(self):
         return 'RelCarth Pos: {} \n\nStd Carth Pos: {} \n'\
             .format(self.rel_carth_pos, self.std_carth_pos)
-
-    def is_dart(self):
-        # Fake implementation to test other functions
-        return True
 
     def get_rel_pos(self):
 
