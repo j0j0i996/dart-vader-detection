b95cd92a059c7a9c07b75029528f5eff68736d1b
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2020-12-05 01:22:47 +0100

    Issue19: Introduce video stream as parallel thread

---
+++ src/videoCapture.py
@@ -0,0 +1,46 @@
+
+# import the necessary packages
+from threading import Thread
+import cv2
+
+rotations = [None, cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+
+class VideoStream:
+    def __init__(self, src=0, width = 640, height = 480, rot = 0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(3, width)
+        self.stream.set(4, height)
+        self.stream.set(cv2.CAP_PROP_EXPOSURE,-3)
+
+        (self.grabbed, self.frame) = self.stream.read()
+        self.rotCode = rotations[int(rot/90)]
+
+        # initialize the variable used to indicate if the thread should
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        if self.rotCode is not None:
+            self.frame = cv2.rotate(self.frame, self.rotCode)
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
